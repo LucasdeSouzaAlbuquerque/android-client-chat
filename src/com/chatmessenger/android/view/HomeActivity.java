63e472c8a615886e7558c755f79017b93c6c1232
nizarattack <nizarattack@gmail.com>
2012-03-19 18:55:40 +0000

    home

---
+++ src/com/chatmessenger/android/view/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.chatmessenger.android.view;
+
+import com.chatmessenger.android.R;
+import com.chatmessenger.android.view.ym.BuddyListActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.home);
+	}
+
+	public void onClickYahoo(View v){
+		Intent intent = new Intent(HomeActivity.this, BuddyListActivity.class);
+		startActivity(intent);
+		
+	}
+
+	public void onClickFacebook(View v){
+		Toast toast = Toast.makeText(HomeActivity.this, "Under Development", Toast.LENGTH_LONG);
+		toast.show();
+	}
+
+	public void onClickGtalk(View v){
+		Intent intent = new Intent(HomeActivity.this, BuddyListActivity.class);
+		startActivity(intent);
+	}
+}
